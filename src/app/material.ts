d04317a04c97cddbc50cbe870d2c6498732c45d4
Dawid Ciesiełkiewicz <d.ciesielkiewicz@gmail.com>
2021-05-07 12:37:16 +0200

    Header navigation

--- src/app/material.ts
+++ src/app/material.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
-const modules = [MatButtonModule, MatIconModule, MatProgressSpinnerModule, MatToolbarModule];
+const modules = [
+  MatButtonModule,
+  MatIconModule,
+  MatListModule,
+  MatProgressSpinnerModule,
+  MatSidenavModule,
+  MatToolbarModule
+];
 
 @NgModule({
   imports: modules,
--- src/app/shared/header/header.component.spec.ts
+++ src/app/shared/header/header.component.spec.ts
@@ -1,13 +1,37 @@
-import { render } from '@testing-library/angular';
+import { fireEvent, render, waitFor } from '@testing-library/angular';
 import { HeaderComponent } from './header.component';
 
 const children = 'children';
 
 describe('HeaderComponent', () => {
   test('Should render children', async () => {
+    const { getByText } = await render(`<app-header>${children}</app-header>`, {
+      declarations: [HeaderComponent]
+    });
+    expect(getByText(children)).toBeInTheDocument();
+  });
+
+  test('Should render toggle menu button', async () => {
     const { getByLabelText } = await render(`<app-header>${children}</app-header>`, {
       declarations: [HeaderComponent]
     });
     expect(getByLabelText('Toggle menu')).toBeInTheDocument();
+  });
+
+  test('Should properly open and close navigation', async () => {
+    const {
+      getByLabelText,
+      getByText,
+      queryByLabelText,
+    } = await render<HeaderComponent>(`<app-header>${children}</app-header>`, {
+      declarations: [HeaderComponent],
+    });
+    expect(queryByLabelText('Sidenav')).not.toBeVisible();
+
+    fireEvent.click(getByLabelText('Toggle menu'));
+    await waitFor(() => expect(getByLabelText('Sidenav')).toBeVisible());
+
+    fireEvent.click(getByText('Home'));
+    await waitFor(() => expect(queryByLabelText('Sidenav')).not.toBeVisible());
   });
 });
